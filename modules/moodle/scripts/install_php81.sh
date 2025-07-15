2cbf3e331478fe94ed344af9549b37f29baae605
BBmobbie <BBmobbie@users.noreply.github.com>
2025-07-15 14:51:52 -0500

    Update and rename install_php74.sh to install_php81.sh

--- modules/moodle/scripts/install_php81.sh
+++ modules/moodle/scripts/install_php81.sh
@@ -3,6 +3,7 @@
 
 yum -y install https://dl.fedoraproject.org/pub/epel/epel-release-latest-$(cat /etc/redhat-release  | sed 's/^[^0-9]*\([0-9]\+\).*$/\1/').noarch.rpm
 yum -y install https://rpms.remirepo.net/enterprise/remi-release-$(cat /etc/redhat-release  | sed 's/^[^0-9]*\([0-9]\+\).*$/\1/').rpm
+
 # Install MySQL Community Edition 8.0
 rpm -ivh https://dev.mysql.com/get/mysql80-community-release-$(uname -r | sed 's/^.*\(el[0-9]\+\).*$/\1/')-1.noarch.rpm
 yum install -y mysql-shell-${mysql_version}
@@ -18,21 +19,22 @@ echo "MySQL Shell successfully installed !"
 
 if [[ $(uname -r | sed 's/^.*\(el[0-9]\+\).*$/\1/') == "el8" ]]
 then
-  dnf -y module enable php:remi-7.4
-  dnf -y install httpd php php-cli php-mysqlnd php-zip php-gd php-mcrypt php-mbstring php-xml php-json php-bcmath php-intl php-xmlrpc php-soap php-opcache
+  dnf -y module enable php:remi-8.1
+  dnf -y install httpd php php-cli php-mysqlnd php-zip php-gd php-mbstring php-xml php-json php-bcmath php-intl php-xmlrpc php-soap php-opcache php-sodium php-curl php-fileinfo php-tokenizer php-ctype php-iconv php-simplexml php-dom php-openssl php-pcre php-spl php-redis
 elif [[ $(uname -r | sed 's/^.*\(el[0-9]\+\).*$/\1/') == "el9" ]]
 then
-  dnf -y install httpd php php-cli php-mysqlnd php-zip php-gd php-mbstring php-xml php-json php-bcmath php-intl php-soap php-opcache php-sodium php-pear php-devel
+  dnf -y module enable php:remi-8.1
+  dnf -y install httpd php php-cli php-mysqlnd php-zip php-gd php-mbstring php-xml php-json php-bcmath php-intl php-soap php-opcache php-sodium php-pear php-devel php-curl php-fileinfo php-tokenizer php-ctype php-iconv php-simplexml php-dom php-openssl php-pcre php-spl php-redis
   pecl config-set preferred_state beta
   pecl install xmlrpc
   echo "extension=xmlrpc.so" > /etc/php.d/50-xmlrpc.ini
   pecl config-set preferred_state stable
 else	
-  yum-config-manager --enable remi-php74
-  yum -y install httpd php php-cli php-mysqlnd php-zip php-gd php-mcrypt php-mbstring php-xml php-json php-bcmath php-intl php-xmlrpc php-soap php-opcache
+  yum-config-manager --enable remi-php81
+  yum -y install httpd php php-cli php-mysqlnd php-zip php-gd php-mbstring php-xml php-json php-bcmath php-intl php-xmlrpc php-soap php-opcache php-sodium php-curl php-fileinfo php-tokenizer php-ctype php-iconv php-simplexml php-dom php-openssl php-pcre php-spl php-redis
 fi
 
-echo "MySQL Shell & PHP successfully installed !"
+echo "MySQL Shell & PHP 8.1 successfully installed !"
 
 yum -y install certbot mod_ssl
 
